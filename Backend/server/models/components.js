579e2301c735f37b3619269a051686dee3a65b50
Priyansh-Parekh <priyanshparekh24@gmail.com>
2025-08-29 21:28:52 +0530

    Schema Upgrade

--- Backend/server/models/components.js
+++ Backend/server/models/components.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
-const componentSchema = new mongoose.Schema({
+const ElectricalComponentSchema = new mongoose.Schema({
     type: {
         type: String,
-        enum: ['resistor', 'capacitor', 'led', 'dc-source', 'ground', 'transistor-npn'],
+        enum:  ['resistor', 'capacitor', 'inductor','dc-source', 'ac-source', 'ground', 'transistor-npn'],
         required: true,
     },
     label: {
@@ -24,12 +24,45 @@ const componentSchema = new mongoose.Schema({
             required: true,
         },
         nodeId: {
-            type: mongoose.Schema.Types.ObjectId, // This will store the unique ID of the node it's connected to.
+            type: mongoose.Schema.Types.ObjectId, 
             default: null,
         }
     }]
 });
 
-const Component = mongoose.model('Component', componentSchema, 'Components');
 
-export default Component;
+const dComponentSchema = new mongoose.Schema({
+
+    type: {
+        type: String,
+        enum: ['and-gate', 'or-gate', 'not-gate', 'xor-gate', 'nand-gate', 'nor-gate'],
+        required: true,
+    },
+    label: { type: String, required: true },
+    position: {
+        x: { type: Number, required: true },
+        y: { type: Number, required: true },
+    },
+    properties: {
+        type: mongoose.Schema.Types.Mixed,
+        default: {}
+    },
+    terminals: [{
+        id: {
+            type: String,
+            required: true,
+        },
+        nodeId: {
+            type: mongoose.Schema.Types.ObjectId, 
+            default: null,
+        }
+    }]
+});
+
+
+const EComponent = mongoose.model('EComponent', ElectricalComponentSchema, 'EComponents');
+
+
+const DComponent = mongoose.model('DComponent', dComponentSchema, 'DComponents');
+
+export default{ EComponent,DComponent};
