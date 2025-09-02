714f6e660914fa71eb041540784633cc0d2fb348
parmarshreya08 <parmarshreya0809@gmail.com>
2025-09-02 15:28:40 +0530

    Added tailwind

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,6 @@
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className="App">
-//         hello
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Workspace from './components/workspace'
+
 
 function App() {
   return (
