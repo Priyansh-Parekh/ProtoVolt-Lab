adef49a15c81aba0e7dc303ac6bafca69e50e261
jake-thomas1 <mathewjake733@gmail.com>
2025-10-02 21:10:39 +0530

    login registration api done along with adding json in app.js

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 
 import express, { json, urlencoded } from 'express';
 const app = express();
+app.use(express.json());
 import cookieParser from 'cookie-parser';
 import connectDB from './server/config/db.js';
 const port = process.env.PORT || 3000;
@@ -12,15 +13,16 @@ connectDB();
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.json()); // This allows the app to accept JSON
 
 // import route files
 import mainRouter from './server/routes/main.js';
 import auth from './server/routes/auth.js';
-
+import userRoutes from './server/routes/userRoutes.js';
 //Routes
 app.use('/', mainRouter);
 app.use('/user/auth/',auth);
-
+app.use('/api/users', userRoutes);
 
 // --- SERVER LISTENER ---
 app.listen(port, () => {
