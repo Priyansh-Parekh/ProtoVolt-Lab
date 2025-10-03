6479a714c4a480befb8e116af7c065b6c42a8d40
Priyansh-Parekh <priyanshparekh24@gmail.com>
2025-10-03 23:32:49 +0530

    Added new page for Creating new Classroom

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -15,6 +15,7 @@ import ClassMembers from './pages/classMembers';
 import Login from './pages/Login.jsx';
 import Signup from './pages/Signup.jsx';
 import OtpVerification from './pages/otpVerification.jsx';
+import CreateClassroom from './pages/createClassroom.jsx';
 
 // We create a wrapper component to ensure Navbar is always present and only content changes
 const MainContent = () => {
@@ -32,6 +33,7 @@ const MainContent = () => {
           <Route path="/" element={<Home />} />
           <Route path="/workspace" element={<WorkspacePage />} />
           <Route path="/classroom" element={<Classroom />} />
+          <Route path="/classroom/create" element={<CreateClassroom />} />
           <Route path="/classroom/class/:id" element={<SpecificClass />} />
           <Route path="/classroom/class/:id/members" element={<ClassMembers />} />
           <Route path="/workspace/new" element={<Workspace />} />
--- Frontend/src/pages/classroom.jsx
+++ Frontend/src/pages/classroom.jsx
@@ -1,24 +1,16 @@
 import React from 'react'
 import { useRef } from 'react';
-// import { useBgMotion } from '../hooks/useBgMotion';
+
 //components
 import ClassroomJoinPopup from '../components/classroom/ClassroomJoinPopup'
-import ClassroomCreatePopup from '../components/classroom/classroomCreatePopup'
 import Classroom_header from '../components/classroom/classroomHeader'
 import Classroom_card from '../components/classroom/classroomCard'
-
-//helpers
-import Loading from '../helper/Loading'
-
-//hooks
-
 
 //seeds
 import {classroomSeed} from '../seeds/data'
 
 //usefull fun
 import { useState } from 'react'
-
 
 const Classroom = () => {
 
@@ -32,13 +24,11 @@ const Classroom = () => {
 //   useBgMotion(containerRef);
 
     const [joinPopup,setJoinPopup] = useState(false);
-    const [createPopup,setCreatePopup] = useState(false);
     return (
         <>
             <div className='h-max min-h-screen bg-[var(--color-primary)] z-[-1] '>
-                 <Classroom_header setJoinPopup = {setJoinPopup} setCreatePopup={setCreatePopup} />
+                 <Classroom_header setJoinPopup = {setJoinPopup} />
                  {joinPopup && <ClassroomJoinPopup setJoinPopup={setJoinPopup} />}
-                 {createPopup && <ClassroomCreatePopup setCreatePopup={setCreatePopup} />}
                 <div className="w-full p-4 md:p-6 lg:p-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                     {classroomSeed.map((course, index) => (
                         <Classroom_card key={index} classroom={course} />
