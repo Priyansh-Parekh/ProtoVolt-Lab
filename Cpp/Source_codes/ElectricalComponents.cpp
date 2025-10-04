3b52f9685325d3fcfc21dad5a31167e93415f6c3
Priyansh-Parekh <priyanshparekh24@gmail.com>
2025-10-04 13:37:40 +0530

    Refined Electrical components

--- Cpp/Source_codes/ElectricalComponents.cpp
+++ Cpp/Source_codes/ElectricalComponents.cpp
@@ -1,25 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class position {
+//----------------- BASIC CLASSES -----------------
+class pos {
 public:
-    int x;
-    int y;
-    position(int x, int y) {
-        this->x = x;
-        this->y = y;
-    }
+    int x, y;
+    pos(int x, int y): x(x), y(y) {}
 };
 
 class nodes {
 private:
     string id;
-    position pos;
+    pos position;
 public:
-    nodes(string id, position pos) {
-        this->id = id;
-        this->pos = pos;
-    }
+    nodes(string id, pos position): id(id), position(position) {}
 };
 
 class terminal {
@@ -27,216 +21,272 @@ private:
     string id;
     nodes node;
 public:
-    terminal(string id, nodes node) {
-        this->id = id;
-        this->node = node;
-    }
+    terminal(string id, nodes node): id(id), node(node) {}
 };
 
-class components {
+//----------------- ELECTRICAL QUANTITIES -----------------
+class voltage {
+public:
+    double value;
+    string unit;
+    void setVoltage(double v, string u){ value = v; unit = u; }
+};
+
+class current {
+public:
+    double value;
+    string unit;
+    void setCurrent(double i, string u){ value = i; unit = u; }
+};
+
+class charge {
+public:
+    double value;
+    string unit;
+    void setCharge(double q, string u){ value = q; unit = u; }
+};
+
+class frequency {
+public:
+    double value;
+    string unit;
+    void setFrequency(double f, string u){ value = f; unit = u; }
+};
+
+class beta{
+public:
+    double value;
+    string unit;
+
+    void setBeta(double b, string u) { this->value = b; this->unit = u; }
+};
+
+class resistance {
+public:
+    double value;
+    string unit;
+    void setResistance(double r, string u){ value = r; unit = u; }
+};
+
+class capacitance {
+public:
+    double value;
+    string unit;
+    void setCapacitance(double c, string u){ value = c; unit = u; }
+};
+
+class inductance {
+public:
+    double value;
+    string unit;
+    void setInductance(double l, string u){ value = l; unit = u; }
+};
+
+//----------------- BASE COMPONENT CLASS -----------------
+class component {
 protected:
     string id;
     string type;
     string label;
-    position pos;
+    pos position;
     vector<terminal> terminals;
 public:
-    void set_data(string id, string type, string label, position pos, vector<terminal> terminals) {
-        this->id = id;
-        this->type = type;
-        this->label = label;
-        this->pos = pos;
-        this->terminals = terminals;
-    }
+    component(string id, string type, string label, pos position, vector<terminal> terminals)
+        : id(id), type(type), label(label), position(position), terminals(terminals) {}
+
     virtual void display() {
         cout << "ID: " << id << " | Type: " << type << " | Label: " << label
-             << " | Position: (" << pos.x << "," << pos.y << ")" << endl;
+             << " | pos: (" << position.x << "," << position.y << ")" << endl;
+    }
+    virtual ~component() = default;  // important for polymorphism
+};
+
+//----------------- RESISTOR -----------------
+class resistor_properties {
+private:
+    resistance R;
+    voltage V;
+    current I;
+public:
+    void show() {
+        cout << "Resistance: " << R.value << " " << R.unit << endl;
+        cout << "Voltage: " << V.value << " " << V.unit << endl;
+        cout << "Current: " << I.value << " " << I.unit << endl;
+    }
+    void setResistance(double r, string u){ R.setResistance(r, u); }
+    void setVoltage(double v, string u){ V.setVoltage(v, u); }
+    void setCurrent(double i, string u){ I.setCurrent(i, u); }
+};
+
+class resistor : public component {
+private:
+    resistor_properties properties;
+public:
+    resistor(string id, string type, string label, pos position, vector<terminal> terminals, resistor_properties props)
+        : component(id, type, label, position, terminals), properties(props) {}
+
+    void display() override {
+        component::display();
+        properties.show();
     }
 };
 
-// Capacitor properties
+//----------------- CAPACITOR -----------------
 class capacitor_properties {
 private:
-    double capacitance;
-    string unit;
+    capacitance C;
+    voltage V;
+    charge Q;
 public:
-    void setCapacitance(double c, string unit) {
-        this->capacitance = c;
-        this->unit = unit;
-    }
     void show() {
-        cout << "Capacitance: " << capacitance << " " << unit << endl;
+        cout << "Capacitance: " << C.value << " " << C.unit << endl;
+        cout << "Voltage: " << V.value << " " << V.unit << endl;
+        cout << "Charge: " << Q.value << " " << Q.unit << endl;
     }
 };
 
-// Capacitor component
-class capacitor : public components {
+class capacitor : public component {
 private:
     capacitor_properties properties;
 public:
-    capacitor(string id, string type, string label, position pos, vector<terminal> terminals, capacitor_properties props) {
-        set_data(id, type, label, pos, terminals);
-        this->properties = props;
-    }
+    capacitor(string id, string type, string label, pos position, vector<terminal> terminals, capacitor_properties props)
+        : component(id, type, label, position, terminals), properties(props) {}
+
     void display() override {
-        components::display();
+        component::display();
         properties.show();
     }
 };
 
-// Inductor properties
+//----------------- INDUCTOR -----------------
 class inductor_properties {
 private:
-    double inductance;
-    string unit;
+    inductance L;
+    current I;
+    voltage V;
 public:
-    void setInductance(double l, string unit) {
-        this->inductance = l;
-        this->unit = unit;
-    }
     void show() {
-        cout << "Inductance: " << inductance << " " << unit << endl;
+        cout << "Inductance: " << L.value << " " << L.unit << endl;
+        cout << "Voltage: " << V.value << " " << V.unit << endl;
+        cout << "Current: " << I.value << " " << I.unit << endl;
     }
 };
 
-// Inductor component
-class inductor : public components {
+class inductor : public component {
 private:
     inductor_properties properties;
 public:
-    inductor(string id, string type, string label, position pos, vector<terminal> terminals, inductor_properties props) {
-        set_data(id, type, label, pos, terminals);
-        this->properties = props;
-    }
+    inductor(string id, string type, string label, pos position, vector<terminal> terminals, inductor_properties props)
+        : component(id, type, label, position, terminals), properties(props) {}
+
     void display() override {
-        components::display();
+        component::display();
         properties.show();
     }
 };
 
-// DC Source properties
+//----------------- DC SOURCE -----------------
 class dcsource_properties {
 private:
-    double voltage;
-    string unit;
+    voltage V;
+    resistance r;
 public:
-    void setVoltage(double v, string unit) {
-        this->voltage = v;
-        this->unit = unit;
-    }
     void show() {
-        cout << "Voltage: " << voltage << " " << unit << endl;
+        cout << "Voltage: " << V.value << " " << V.unit << endl;
+        cout << "Internal Resistance: " << r.value << " " << r.unit << endl;
     }
 };
 
-// DC Source component
-class dc_source : public components {
+class dc_source : public component {
 private:
     dcsource_properties properties;
 public:
-    dc_source(string id, string type, string label, position pos, vector<terminal> terminals, dcsource_properties props) {
-        set_data(id, type, label, pos, terminals);
-        this->properties = props;
-    }
+    dc_source(string id, string type, string label, pos position, vector<terminal> terminals, dcsource_properties props)
+        : component(id, type, label, position, terminals), properties(props) {}
+
     void display() override {
-        components::display();
+        component::display();
         properties.show();
     }
 };
 
-// AC Source properties
+//----------------- AC SOURCE -----------------
 class acsource_properties {
 private:
-    double voltage;
-    double frequency;
-    string unit;
+    voltage V;
+    frequency f;
 public:
-    void setValues(double v, double f, string unit) {
-        this->voltage = v;
-        this->frequency = f;
-        this->unit = unit;
-    }
     void show() {
-        cout << "Voltage: " << voltage << " " << unit
-             << " | Frequency: " << frequency << " Hz" << endl;
+        cout << "Voltage: " << V.value << " " << V.unit << endl;
+        cout << "Frequency: " << f.value << " " << f.unit << endl;
     }
 };
 
-// AC Source component
-class ac_source : public components {
+class ac_source : public component {
 private:
     acsource_properties properties;
 public:
-    ac_source(string id, string type, string label, position pos, vector<terminal> terminals, acsource_properties props) {
-        set_data(id, type, label, pos, terminals);
-        this->properties = props;
-    }
+    ac_source(string id, string type, string label, pos position, vector<terminal> terminals, acsource_properties props)
+        : component(id, type, label, position, terminals), properties(props) {}
+
     void display() override {
-        components::display();
+        component::display();
         properties.show();
     }
 };
 
-// Ground component
-class ground : public components {
+//----------------- GROUND -----------------
+class ground : public component {
 public:
-    ground(string id, string type, string label, position pos, vector<terminal> terminals) {
-        set_data(id, type, label, pos, terminals);
-    }
+    ground(string id, string type, string label, pos position, vector<terminal> terminals)
+        : component(id, type, label, position, terminals) {}
+
     void display() override {
-        components::display();
+        component::display();
         cout << "Ground component" << endl;
     }
 };
 
-// Transistor NPN properties
+//----------------- TRANSISTOR NPN -----------------
 class transistor_npn_properties {
 private:
-    double beta;
-    string unit;
+   beta B;
 public:
-    void setBeta(double b, string unit) {
-        this->beta = b;
-        this->unit = unit;
-    }
     void show() {
-        cout << "Beta: " << beta << " " << unit << endl;
+        cout << "Beta: " << B.value << " " << B.unit << endl;
     }
 };
 
-// Transistor NPN component
-class transistor_npn : public components {
+class transistor_npn : public component {
 private:
     transistor_npn_properties properties;
 public:
-    transistor_npn(string id, string type, string label, position pos, vector<terminal> terminals, transistor_npn_properties props) {
-        set_data(id, type, label, pos, terminals);
-        this->properties = props;
-    }
+    transistor_npn(string id, string type, string label, pos position, vector<terminal> terminals, transistor_npn_properties props)
+        : component(id, type, label, position, terminals), properties(props) {}
+
     void display() override {
-        components::display();
+        component::display();
         properties.show();
     }
 };
 
-//CIRCUIT CLASS 
+//----------------- CIRCUIT -----------------
 class Circuit {
 private:
     string name;
     bool analyzed;
-    vector<shared_ptr<Component>> components;  // store mixed components
+    vector<shared_ptr<component>> comps;  // store mixed component
 public:
     Circuit(string name): name(name), analyzed(false) {}
 
-    void addComponent(shared_ptr<Component> comp) {
-        components.push_back(comp);
+    void addComponent(shared_ptr<component> comp) {
+        comps.push_back(comp);
     }
 
     void showCircuit() {
         cout << "Circuit: " << name << endl;
-        for(auto &c : components) {
-            c->display();
-        
+        for(auto &c : comps) {
+            c->display(); // virtual dispatch
         }
     }
 };
